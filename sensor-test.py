3887baef9ab7e941887a8cac24b1fef0e068c3fd
Joe Barrett <joebarrett401@gmail.com>
2018-09-09 13:09:49 +0100

    Changed sensor test script to use gpizero spi rather than adafruit

--- sensor-test.py
+++ sensor-test.py
@@ -1,26 +1,14 @@
 import gpiozero
+import warnings
 from time import sleep
-import Adafruit_MCP3008
-import Adafruit_GPIO.SPI as SPI
 
-SPI_PORT = 0
-SPI_DEVICE = 0
-mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
-sensor = gpiozero.DigitalOutputDevice(4)
+warnings.filterwarnings('ignore')
+sensor = gpiozero.MCP3008(channel=0)
+sensor_power = gpiozero.DigitalOutputDevice(4)
 
-sensor.on()
-print('Reading MCP3008 values, press Ctrl-C to quit...')
-# Print nice channel column headers.
-print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
-print('-' * 57)
-# Main program loop.
 while True:
-    # Read all the ADC channel values in a list.
-    values = [0]*8
-    for i in range(8):
-        # The read_adc function will get the value of the specified channel (0-7).
-        values[i] = mcp.read_adc(i)
-    # Print the ADC values.
-    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
-    # Pause for half a second.
-    sleep(0.5)
+    sensor_power.on()
+    sleep(0.02)
+    print(sensor.value)
+    sensor_power.off()
+    sleep(10)
